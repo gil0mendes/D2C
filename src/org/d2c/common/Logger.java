a015efa39a194aefa4533514716863efa557a1d1
Gil Mendes <gil00mendes@gmail.com>
2015-03-27 15:23:42 +0000

    Improve the Logger class

--- src/org/d2c/common/Logger.java
+++ src/org/d2c/common/Logger.java
@@ -1,9 +1,10 @@
 package org.d2c.common;
 
 import java.io.IOException;
-import java.util.logging.FileHandler;
-import java.util.logging.Level;
-import java.util.logging.SimpleFormatter;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.*;
 
 public class Logger {
 
@@ -82,6 +83,13 @@ public class Logger {
     public static void enable()
     {
         print = true;
+
+        log.setUseParentHandlers(false);
+
+        MyFormatter formatter = new MyFormatter();
+        ConsoleHandler handler = new ConsoleHandler();
+        handler.setFormatter(formatter);
+        log.addHandler(handler);
     }
 
     public static void disable()
@@ -109,4 +117,30 @@ public class Logger {
         return DEBUG_LEVEL;
     }
 
+    static class MyFormatter extends Formatter {
+        //
+        // Create a DateFormat to format the logger timestamp.
+        //
+        private static final DateFormat df = new SimpleDateFormat("dd/MM/yyyy hh:mm:ss.SSS");
+
+        public String format(LogRecord record)
+        {
+            StringBuilder builder = new StringBuilder(1000);
+            builder.append(df.format(new Date(record.getMillis()))).append(" - ");
+            builder.append("[").append(record.getLevel()).append("] - ");
+            builder.append(formatMessage(record));
+            builder.append("\n");
+            return builder.toString();
+        }
+
+        public String getHead(Handler h)
+        {
+            return super.getHead(h);
+        }
+
+        public String getTail(Handler h)
+        {
+            return super.getTail(h);
+        }
+    }
 }
